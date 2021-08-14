6a45acf9ad1145166fd84b1653767406c6e84597
samuelecisaro <samuele.cisaro@hotmail.it>
2021-08-15 01:47:10 +0200

    initial commit

---
+++ contracts/ArtWork.sol
@@ -0,0 +1,26 @@
+// contracts/ArtWork.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Artwork is ERC721URIStorage {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  constructor() ERC721("ARTWORK", "ARW") {}
+
+  function createNewItem(string memory tokenURI, address owner) 
+  public 
+  returns(uint256)
+  {
+    _tokenIds.increment();
+
+    uint256 newItemId = _tokenIds.current();
+    _mint(owner, newItemId);
+    _setTokenURI(newItemId, tokenURI);
+
+    return newItemId;
+  }
+}
