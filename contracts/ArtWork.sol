bfdaf74173c25140e83bc3e51e35c5fe68af43f0
samuelecisaro <samuele.cisaro@hotmail.it>
2021-08-20 20:37:55 +0200

    feat: add possibility to get the actual item id

--- contracts/ArtWork.sol
+++ contracts/ArtWork.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
 
 contract Artwork is ERC721URIStorage {
   using Counters for Counters.Counter;
@@ -22,5 +23,13 @@ contract Artwork is ERC721URIStorage {
     _setTokenURI(newItemId, tokenURI);
 
     return newItemId;
+  }
+
+  function getTokenCounter() 
+  public
+  view
+  returns(uint256)
+  {
+    return _tokenIds.current();
   }
 }
